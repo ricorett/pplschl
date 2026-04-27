fb969ddb3edbe5068930e878b0717c8d951fc5c8
Lana <115147586+ricorett@users.noreply.github.com>
2026-04-27 14:19:59 +0300

    add fixes to third task

--- task3/main.go
+++ task3/main.go
@@ -1,50 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	usdEur = 1.17
+	usdRub = 75.19
+	eurRub = usdRub * usdEur
+)
 
 func main() {
-	const (
-		usdEur = 1.17
-		usdRub = 75.19
-	)
-	//eurRub := usdRub * usdEur
+
 	//fmt.Printf("%.2f\n ", eurRub)
+
+	var inputSrc, inputDist string
+	var number float64
+	var err error
 	for {
-		err := getUserInput()
+		inputSrc, number, inputDist, err = getUserInput()
 		if err != nil {
 			fmt.Println("Error:", err)
-			err = getUserInput()
+			inputSrc, number, inputDist, err = getUserInput()
+		} else {
+			break
+		}
+	}
+	calc := calcCurrency(inputSrc, number, inputDist)
+	fmt.Printf("%.1f %s", calc, inputDist)
+
+}
+func calcCurrency(inputSrc string, number float64, inputDist string) float64 {
+	var calculation float64
+	switch inputDist {
+	case "eur":
+		switch inputSrc {
+		case "usd":
+			calculation = number * usdEur
+		case "rub":
+			calculation = number * usdRub
+		}
+	case "usd":
+		switch inputSrc {
+		case "eur":
+			calculation = number * usdEur
+		case "rub":
+			calculation = number * usdRub
 		}
 
+	case "rub":
+		switch inputSrc {
+		case "eur":
+			calculation = number * eurRub
+		case "usd":
+			calculation = number * usdRub
+		}
+	default:
+		fmt.Println("Error")
 	}
+
+	return calculation
 }
 
-//func calcCurrency(num float64, firstConv float64, secondConv float64) (float64, float64) {
-//
-//}
-
-func getUserInput() error {
+func getUserInput() (string, float64, string, error) {
 
 	fmt.Print("Введите исходную валюту(USD, EUR, RUB): ")
 	var input string
 	_, err := fmt.Scanln(&input)
 	if err != nil {
-		return err
+		return "", 0, "", err
 	}
+	input = strings.ToLower(input)
 
 	var number float64
 	fmt.Print("Введите сумму: ")
 	_, err = fmt.Scanln(&number)
 	if err != nil {
-		return err
+		return "", 0, "", err
 	}
 
 	fmt.Print("Введите целевую валюту(USD, EUR, RUB): ")
 	var inputDist string
 	_, err = fmt.Scanln(&inputDist)
 	if err != nil {
-		return err
+		return "", 0, "", err
 	}
+	inputDist = strings.ToLower(inputDist)
 
-	return nil
+	return input, number, inputDist, nil
 }
