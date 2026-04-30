b5ef836b7bda91707fefdc3278ab381fcfb3755c
Lana <115147586+ricorett@users.noreply.github.com>
2026-04-30 14:18:54 +0300

    reverse array

---
+++ project4/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a)
+	fmt.Println(a)
+	var p *int
+	fmt.Println(p)
+}
+
+func reverse(arr *[4]int) {
+	for index, value := range *arr {
+		(*arr)[len(*arr)-1-index] = value
+	}
+}
