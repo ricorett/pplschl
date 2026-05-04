fefcbb6bdfaef7d1c3b52aa93cdbf5512a90cafa
Lana <115147586+ricorett@users.noreply.github.com>
2026-05-04 20:25:29 +0300

    couldn't resolve

--- task7/storage/storage.go
+++ task7/storage/storage.go
@@ -1,1 +1,27 @@
 package storage
+
+import (
+	_ "bins/bin"
+	"encoding/json"
+	"os"
+)
+
+func SaveBin(BinList List, filename string) error {
+	file, err := json.Marshal(BinList)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, file, 0644)
+}
+
+func ReadBin(BinList List, filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(file, &BinList)
+	if err != nil {
+		return err
+	}
+	return nil
+}
