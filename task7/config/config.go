df9fc746c698dfbeccb2c4987c12dd501eb6dd5e
Lana <115147586+ricorett@users.noreply.github.com>
2026-05-07 14:28:58 +0300

    add config into package api

--- task7/config/config.go
+++ task7/config/config.go
@@ -9,11 +9,12 @@ type Env struct{
 	Key string
 }
 
-func ReadEnv(){
+func ReadEnv() (*Env, error){
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Print("Не удалость найти env файл")
 	}
 	var e  Env
 	e.Key = os.Getenv("KEY")
+	return &e, nil
 }
